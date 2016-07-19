701274c6d830af54b4571e538f01e772d1c6a5e1
Kyle Ondy <kyle@ondy.me>
2016-07-19 11:51:32 -0400

    allow to deploy to different branches

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,22 @@
 set -eu
 DRONE_KEY=/tmp/drone_key
 
+if [ $1 == "staging" ]
+then
+  BRANCH=staging
+else
+  if [ $1 == "production" ]
+  then BRANCH=production
+  else
+    echo Not a valid branch
+    exit 1
+  fi
+fi
+
+echo Deploying $BRANCH
+
 rm -f $DRONE_KEY
 echo "$RSYNC_PRIVATE_KEY" > $DRONE_KEY
 chmod 400 $DRONE_KEY
-rsync -avP -e"ssh -p 3220 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /tmp/drone_key" _output/latest.tar.gz root@static.ondy.me:/data/kyleondy.com/
+rsync -avP -e"ssh -p 3220 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /tmp/drone_key" _output/latest.tar.gz root@static.ondy.me:/data/kyleondy.com/$BRANCH.tar.gz
 curl -X POST $DOCKER_HUB_HOOK
