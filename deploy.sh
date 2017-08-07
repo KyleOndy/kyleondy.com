4baf18401b282e442e0e0825fad11de255b8806a
Kyle Ondy <kyle@ondy.me>
2017-08-07 15:38:07 -0400

    add manual deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,17 @@
 set -euo pipefail
 
 BRANCH=${1:-master}
+IMAGE=kyleondy/website:$BRANCH
+
+
+make clean && make check && make package
 cp _output/$BRANCH.tar.gz ./docker/site.tar.gz
-docker build docker/ -t kyleondy/website:$BRANCH
+docker build docker -t $IMAGE
+
+if [ $BRANCH == "master" ]
+then
+  IMAGE=kyleondy/website:production
+  docker tag kyleondy/website:master $IMAGE
+fi
+
+docker push $IMAGE
