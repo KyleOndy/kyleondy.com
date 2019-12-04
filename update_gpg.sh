45a735cad463612a0a7452f7e9d3a3c2223ece58
Kyle Ondy <kyle@ondy.org>
2019-12-03 20:46:28 -0500

    Add ability for gpg update to take local file.

--- update_gpg.sh
+++ update_gpg.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
-key_file="$(mktemp -d)/key.gpg"
-# get key from keyserver
+key_file=
+if [ -f "$1" ]; then
+  key_file="$1"
+else
+  key_file="$(mktemp -d)/key.gpg"
+  echo "key_file: $key_file"
+  # get key from keyserver
+  curl -so "$key_file" "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3c799d26057b64e6d907b0acdb0e3c33491f91c9"
 
-curl -so "$key_file" "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3c799d26057b64e6d907b0acdb0e3c33491f91c9"
+  # update static file
+fi
 
-# update static file
 cp "$key_file" ./provider/static/pgp.txt
 
 # update pgp page
