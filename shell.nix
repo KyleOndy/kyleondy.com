915b0c299228f88ec40e4a886a00d0cf2dbba1c1
Kyle Ondy <kyle@ondy.org>
2020-12-08 20:05:57 -0500

    cleanup

--- shell.nix
+++ shell.nix
@@ -10,7 +10,6 @@ stdenv.mkDerivation {
   ];
 
   shellHook = ''
-    export PATH="$HOME/.gem/ruby/2.6.0/bin:$PATH"
     export AWS_ACCESS_KEY_ID="AKIAW4WHQVVUXSIPY4YK"
     export AWS_SECRET_ACCESS_KEY="$(pass show aws.amazon.com/keys/AKIAW4WHQVVUXSIPY4YK)"
   '';
