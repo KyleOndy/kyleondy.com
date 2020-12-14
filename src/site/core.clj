5ecda877bec84bd4130fe3f2972e16981918f8b4
Kyle Ondy <kyle@ondy.org>
2020-12-14 09:43:09 -0500

    more

--- src/site/core.clj
+++ src/site/core.clj
@@ -1,6 +1,22 @@
 (ns site.core)
+ (use 'hiccup.core)
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+;; defining data for the menu
+(def menu-items
+  ["Crab Rangoon"
+   "Pot Stickers"
+   "Miso Soup"])
+
+;; creating a function to generate a list elment from the provided `menu-item`
+(defn create-menu-item [menu-item]
+  [:li.menu-item menu-item])
+
+;; generating an unordered list by mapping over the `menu-items` data
+(def menu
+  [:ul
+   (map create-menu-item menu-items)])
